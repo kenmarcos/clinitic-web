b8be3bdb430bab6c56c83e5e80ce619ae9be920c
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-11-28 17:59:19 -0300

    add .env

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8000/api",
-  baseURL: "https://clinitic-api.herokuapp.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
